4401b9ffeded8daa4ce4c7f22d19bc838c9f4e5b
Vidhi Gaydhane <120549998+VIDHI-tech@users.noreply.github.com>
2023-06-18 19:20:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-//your JS code here. If required.
-var line = document.getElementById('line');
-var angle = 0;
+// //your JS code here. If required.
+// var line = document.getElementById('line');
+// var angle = 0;
 
-function rotateLine() {
-   angle += 1;
-   line.style.transform = 'rotate(' + angle + 'deg)';
-   requestAnimationFrame(rotateLine);
-}
+// function rotateLine() {
+//    angle += 1;
+//    line.style.transform = 'rotate(' + angle + 'deg)';
+//    requestAnimationFrame(rotateLine);
+// }
 
-rotateLine();
+// rotateLine();
